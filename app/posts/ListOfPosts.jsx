592721dd8f4b7a4cf80d1b991cbbbaff39f84a20
jonas-code <alertcodes30@gmail.com>
2023-08-15 12:09:57 +0200

    class server-side-render + link

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,7 +1,16 @@
 import { LikeButton } from '../LikeButton'
+import Link from 'next/link'
 
 const fetchPosts = () => {
-  return fetch('https://jsonplaceholder.typicode.com/posts')
+  // Nota: evitem que generi un contingut static (un html amb les dades del fetch) en el moment
+  // de executar npm run build
+  // Basicament li estem dient que volem que sigui server-side-rendered
+  // Tenim el static, render-side-rendered ({ cache: 'no-store' }) o regenerate cada x temps (next: { revalidate: 60 })
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60
+    }
+  })
     .then(res => res.json())
 }
 export default async function ListOfPosts () {
@@ -10,9 +19,11 @@ export default async function ListOfPosts () {
   return (
     posts.slice(0, 5).map(post => (
       <article key={post.id}>
-        <h1>{post.title}</h1>
-        <p>{post.body}</p>
-        <LikeButton id={post.id} />
+        <Link href='/posts/[id]' as={`/posts/${post.id}`}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          <LikeButton id={post.id} />
+        </Link>
       </article>
     ))
   )
