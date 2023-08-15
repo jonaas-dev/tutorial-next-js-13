5c5d02e7cb0f000d401eb0353e975689a94b51c2
jonas-code <alertcodes30@gmail.com>
2023-08-15 13:01:23 +0200

    alternativa a loading, suspense

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,7 +1,9 @@
 import ListOfPosts from './ListOfPosts.jsx'
+import { Suspense } from 'react'
 export default async function PostsPage ({ params }) {
   return (
     <section>
+      <Suspense fallback={<p>Loading...</p>} />
       <ListOfPosts />
     </section>
   )
