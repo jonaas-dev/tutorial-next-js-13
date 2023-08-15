c829e0ee2c072115e4622ca33f310f5c69244f9d
jonas-code <alertcodes30@gmail.com>
2023-08-15 12:20:28 +0200

    add docu

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,11 +1,17 @@
 import { LikeButton } from '../LikeButton'
 import Link from 'next/link'
 
+// NOTES (next 12 to 13)
+// getStaticProps (genera static content)
+// return fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+
+// getServerSideProps (server-side-rendered)
+// return fetch('https://jsonplaceholder.typicode.com/posts', cache: 'no-store').then(res => res.json())
+
+// incremental static regeneration (regenerate cada x temps)
+// return fetch('https://jsonplaceholder.typicode.com/posts', next: { revalidate: 60 }).then(res => res.json())
+
 const fetchPosts = () => {
-  // Nota: evitem que generi un contingut static (un html amb les dades del fetch) en el moment
-  // de executar npm run build
-  // Basicament li estem dient que volem que sigui server-side-rendered
-  // Tenim el static, render-side-rendered ({ cache: 'no-store' }) o regenerate cada x temps (next: { revalidate: 60 })
   return fetch('https://jsonplaceholder.typicode.com/posts', {
     next: {
       revalidate: 60
